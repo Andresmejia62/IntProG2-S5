2b6273126fab07a32a4b6499723a86711037c77f
Andresmejia62 <amejia@uamv.edu.ni>
2025-04-25 08:32:03 -0600

    Ejercicio3

---
+++ sesion3/ejercicio3.py
@@ -0,0 +1,22 @@
+print("Presiona 0 para salir")
+dia = int(input("Ingrese el dia de la semana de forma númerica(1-7)): "))
+
+while dia != 0:
+    if dia == 1:
+        print("Lunes")
+    elif dia == 2:
+        print("Martes")
+    elif dia ==3:
+        print("Miercoles")
+    elif dia ==4:
+        print("Jueves")
+    elif dia ==5:
+        print("Viernes")
+    elif dia==6:
+        print("Sabado")
+    elif dia==7:
+        print("Domingo")
+    else:
+        print("Error")
+print("Presiona 0 para salir")
+dia = int(input("Ingrese el dia de la semana de forma númerica(1-7)): "))
